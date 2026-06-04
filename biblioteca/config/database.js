1c0a0979d57a6841686e853a34276d178f646b93
Angel Gabriel <antimagia132@gmail.com>
2026-06-04 00:16:25 -0500

    conexión a la base de datos con el nombre: biblioteca-desarrollo-app-web-pa1c2

--- biblioteca/config/database.js
+++ biblioteca/config/database.js
@@ -1,1 +1,18 @@
 
+import { Sequelize } from "sequelize";
+
+const sequelize = new Sequelize("biblioteca-desarrollo-app-web-pa1c2", "root", "", {
+    host: "localhost",
+    dialect: "mysql",
+    port: 3306,
+    logging: false
+});
+
+try {
+    await sequelize.authenticate();
+    console.log("Conexión con MySQL exitosa");
+} catch (error) {
+    console.error("Error de conexión:", error);
+}
+
+export default sequelize;
