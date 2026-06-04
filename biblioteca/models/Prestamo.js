a932934b75ece44faebc1dff4b4487fe5af3f387
VmesterV <76802635@continental.edu.pe>
2026-06-04 18:51:08 -0500

    Create Prestamo

---
+++ biblioteca/models/Prestamo.js
@@ -0,0 +1,36 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/database.js";
+
+const Prestamo = sequelize.define("Prestamo", {
+	id: {
+		type: DataTypes.INTEGER,
+		primaryKey: true,
+		autoIncrement: true
+	},
+	usuario_id: {
+		type: DataTypes.INTEGER,
+		allowNull: false
+	},
+	libro_id: {
+		type: DataTypes.INTEGER,
+		allowNull: false
+	},
+	fecha_prestamo: {
+		type: DataTypes.DATE,
+		allowNull: false
+	},
+	fecha_devolucion: {
+		type: DataTypes.DATE,
+		allowNull: true
+	},
+	estado: {
+		type: DataTypes.ENUM("pendiente", "prestado", "devuelto"),
+		allowNull: false,
+		defaultValue: "pendiente"
+	}
+}, {
+	timestamps: false
+});
+
+export default Prestamo;
+
